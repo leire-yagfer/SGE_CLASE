bde3dec3b365095c59c177a10fcdb3cbe49c9fab
leire-yagfer <leire.yagfer.1@educa.jcyl.es>
2024-10-17 19:08:00 +0200

    Delete pythonSGE directory

--- pythonSGE/07excepciones/excepciones.py
+++
@@ -1,6 +0,0 @@
-try:
-    print(10/10)
-except:
-    print("Se ha producido un error")
-finally: #SIEMPRE que se hagan excepciones, tras acabar, pone lo que se escriba
-    print("Ha finalizado el manejo de excepciones") 
